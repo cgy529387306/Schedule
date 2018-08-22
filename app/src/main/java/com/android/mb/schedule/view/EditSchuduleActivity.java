6a2f8134891fbf16fdb77fb51ed967ffc66d8fd3
chenqm <593960111@qq.com>
2018-08-22 18:56:56 +0800

    首页、侧滑栏、底部导航栏

--- app/src/main/java/com/android/mb/schedule/view/EditSchuduleActivity.java
+++ app/src/main/java/com/android/mb/schedule/view/EditSchuduleActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.mb.schedule.R;
@@ -20,6 +21,13 @@ public class EditSchuduleActivity extends BaseActivity implements View.OnClickLi
     private TextView mBtnLocation; //定位
     private TextView mTvAddress ; //位置
     private TextView mTvScheduleContent; // 日程内容
+    private TextView mBtnChangeDocument ; //点击替换附件
+    private TextView mTvStartDate ; //开始日期
+    private TextView mTvStartTime ; //开始时间
+    private TextView mTvEndDate ; //结束日期
+    private TextView mTvEndTime ; //结束时间
+    private ImageView mIvRemind ; //全天提醒
+    private boolean isRemind = false;
 
     @Override
     protected void loadIntent() {
@@ -42,6 +50,12 @@ public class EditSchuduleActivity extends BaseActivity implements View.OnClickLi
         mBtnLocation = findViewById(R.id.tv_location);
         mTvAddress = findViewById(R.id.tv_address);
         mTvScheduleContent = findViewById(R.id.tv_schedulecontent);
+        mBtnChangeDocument = findViewById(R.id.tv_changedocument);
+        mTvStartDate = findViewById(R.id.tv_startdate);
+        mTvStartTime = findViewById(R.id.tv_starttime);
+        mTvEndDate = findViewById(R.id.tv_enddate);
+        mTvEndTime = findViewById(R.id.tv_endtime);
+        mIvRemind = findViewById(R.id.iv_remind);
     }
 
     @Override
@@ -52,12 +66,18 @@ public class EditSchuduleActivity extends BaseActivity implements View.OnClickLi
     @Override
     protected void setListener() {
         mBtnLocation.setOnClickListener(this);
+        mBtnChangeDocument.setOnClickListener(this);
+        mIvRemind.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.tv_location){
+        }else  if (id == R.id.tv_changedocument){
+        }else  if (id == R.id.iv_remind){
+            isRemind = !isRemind;
+            mIvRemind.setImageResource(isRemind?R.mipmap.ic_vibrate_open:R.mipmap.ic_vibrate_close);
         }
     }
 
